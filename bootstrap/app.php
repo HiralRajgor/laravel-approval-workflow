025c20669b92e165348ffb7f1d16c2a3923234fa
Hiral Rajgor <hiralrajgor254@gmail.com>
2026-03-22 00:25:35 +0530

    fixed CI workflow

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\Authenticate;
+use App\Providers\EventServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,6 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withProviders([
-        App\Providers\EventServiceProvider::class,
+        EventServiceProvider::class,
     ])
     ->create();
